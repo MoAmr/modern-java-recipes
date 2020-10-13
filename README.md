5823294c5f95aa07adb238a79ad15a515dfb38f3
Mohammed Amr <haveagoodday.mido@gmail.com>
2020-10-14 00:05:26 +0200

    Lambda expression implementing FilenameFilter. closes #2

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# modern-java-recipes
+--------------------------------------------------------------------------
+
+# modern-java-recipes:
+
+--------------------------------------------------------------------------
+
+# Lambda Expressions:-
+ 
+<h3><u>Note:</u></h3><h4>A functional interface is an interface with a single abstract
+method (SAM). A class implements any interface by providing implementations 
+for all the methods in it. This can be done with a top-level class, an inner
+class, or even an anonymous inner class.</h4>
+
+<h3><u>Note:</u></h3><h4>A lambda expression must match the argument types and return
+type in the signature of the single abstract method in the interface. 
+This is called being compatible with the method signature. The lambda expression
+is thus the implementation of the interface method, and can also be assigned to a
+reference of that interface type.</h4>
+
+<h3><u>Note:</u></h3><h4>Lambda expressions can only be assigned to
+functional interface references.</h4>
+
+<h3><u>Note:</u></h3><h4>Lambda expressions never exist alone. 
+There is always a context for the expression, which indicates the 
+functional interface to which the expression is assigned.
+A lambda can be an argument to a method, a return type from a method,
+or assigned to a reference. In each case, the type of the assignment 
+must be a functional interface.</h4>
+
+--------------------------------------------------------------------------
--- src/LambdaExpressions/Main.java
+++ src/LambdaExpressions/Main.java
@@ -1,5 +1,9 @@
 package LambdaExpressions;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+
 /**
  * @author Mohammed Amr
  * @created 13/10/2020 - 22:36
@@ -9,9 +13,42 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Assigning Lambda to a variable
+        /** Assigning a lambda expression to a variable */
         Runnable r = () -> System.out.println("Inside Thread constructor using lambda");
         new Thread(r).start();
-        
+
+        /** Implements FilenameFilter using an anonymous inner class to return only Java source files. */
+        File directory = new File("./resources");
+
+//        String[] names = directory.list(new FilenameFilter() {
+//            @Override
+//            public boolean accept(File dir, String name) {
+//                return name.endsWith(".txt");
+//            }
+//        });
+//
+//        System.out.println(Arrays.asList(names));
+
+        // Note: In this case, the accept method returns true if the filename ends with .txt and false otherwise.
+
+        /** Lambda expression implementing FilenameFilter */
+
+        // Note: This time the arguments are contained within parentheses, but do not have types declared.
+        // At compile time the compiler knows that the list method takes an argument of type FilenameFilter,
+        // and therefore knows the signature of its single abstract method (accept).
+
+//        String[] names = directory.list((dir, name) -> name.endsWith(".txt"));
+//        System.out.println(Arrays.asList(names));
+
+        /** A block lambda */
+
+        // Note: This is known as a block lambda. In this case the body still consists of a single line,
+        // but the braces now allow for multiple statements. The return keyword is now required.
+
+        String[] names = directory.list((File dir, String name) -> {
+            return name.endsWith(".txt");
+        });
+
+        System.out.println(Arrays.asList(names));
     }
 }
