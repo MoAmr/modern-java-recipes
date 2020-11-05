1da79a4563f72a911926700c8a71aaa65a5922a7
Mohammed Amr <haveagoodday.mido@gmail.com>
2020-11-05 23:24:40 +0200

    Sorting streams using length comparator. closes #50

--- src/ComparatorsAndCollectors/Main.java
+++ src/ComparatorsAndCollectors/Main.java
@@ -2,8 +2,11 @@ package ComparatorsAndCollectors;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * @author Mohammed Amr
@@ -14,7 +17,7 @@ public class Main {
 
     private List<String> sampleStrings = Arrays.asList("this", "is", "a", "list", "of", "strings");
 
-    /** Sorting strings lexicographically */
+    /** Sorting strings lexicographically - START */
     public List<String> defaultSort() {
         Collections.sort(sampleStrings); // Default sort from Java 7 and below
         return sampleStrings;
@@ -23,8 +26,23 @@ public class Main {
     public List<String> defaultSortUsingStreams() {
         return sampleStrings.stream()
                 .sorted() // Default sort from Java 8 and above
-                .collect(Collectors.toList());
+                .collect(toList());
     }
+    /** Sorting strings lexicographically - END */
+
+    /** Sorting streams using length comparator - START */
+    public List<String> lengthSortUsingSorted() {
+        return sampleStrings.stream()
+                .sorted((s1, s2) -> s1.length() - s2.length()) // Using a lambda for the Comparator to sort by length
+                .collect(toList());
+    }
+
+    public List<String> lengthSortUsingComparator() {
+        return sampleStrings.stream()
+                .sorted(Comparator.comparingInt(String::length)) // Using a Comparator using the comparingInt method
+                .collect(toList());
+    }
+    /** Sorting streams using length comparator - END */
 
     public static void main(String[] args) {
 
