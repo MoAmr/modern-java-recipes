3ae1ef20377b92344dff8de64c9b488e720e6668
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-01-17 23:38:42 +0200

    Composing two Futures together. closes #145

--- src/parallelism_and_concurrency/Main.java
+++ src/parallelism_and_concurrency/Main.java
@@ -168,5 +168,17 @@ public class Main {
                 .isParallel());
     }
 
+    /** Composing two Futures together */
+    @Test
+    public void compose() throws Exception {
+        int x = 2;
+        int y = 3;
+        CompletableFuture<Integer> completableFuture =
+                CompletableFuture.supplyAsync(() -> x)
+                .thenCompose(n -> CompletableFuture.supplyAsync(() -> n + y));
+
+        assertTrue(5 == completableFuture.get());
+    }
+
 
 }
