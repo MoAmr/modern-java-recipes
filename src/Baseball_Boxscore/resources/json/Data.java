425c747c09fd952d5449765ad30ff5a99de2dae8
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-01-19 02:21:58 +0200

    Adding Data Class. closes #152

---
+++ src/Baseball_Boxscore/resources/json/Data.java
@@ -0,0 +1,19 @@
+package Baseball_Boxscore.resources.json;
+
+/**
+ * @author Mohammed Amr
+ * @created 19/01/2021 - 02:21
+ * @project Modern Java Recipes
+ */
+
+public class Data {
+    private Boxscore boxscore;
+
+    public Boxscore getBoxscore() {
+        return boxscore;
+    }
+
+    public void setBoxscore(Boxscore boxscore) {
+        this.boxscore = boxscore;
+    }
+}
