1ba627009d9419bc47f2ee55c699bebe61ded019
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-01-19 02:27:27 +0200

    Adding Linescore Class. closes #153

---
+++ src/Baseball_Boxscore/resources/json/Linescore.java
@@ -0,0 +1,33 @@
+package Baseball_Boxscore.resources.json;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @author Mohammed Amr
+ * @created 19/01/2021 - 02:25
+ * @project Modern Java Recipes
+ */
+public class Linescore {
+
+    @SerializedName("home_team_runs")
+    private String homeTeamRuns;
+
+    @SerializedName("away_team_runs")
+    private String awayTeamRuns;
+
+    public String getHomeTeamRuns() {
+        return homeTeamRuns;
+    }
+
+    public void setHomeTeamRuns(String homeTeamRuns) {
+        this.homeTeamRuns = homeTeamRuns;
+    }
+
+    public String getAwayTeamRuns() {
+        return awayTeamRuns;
+    }
+
+    public void setAwayTeamRuns(String awayTeamRuns) {
+        this.awayTeamRuns = awayTeamRuns;
+    }
+}
